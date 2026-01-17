47ec0c9d5a14338fe1f90cd780ee672ead1f93cd
Jontae Hall <141603361+JontaeHall@users.noreply.github.com>
2026-01-17 01:59:46 -0500

    Update Dolphin.java

--- src/Dolphin.java
+++ src/Dolphin.java
@@ -1,24 +1,4 @@
-/** TODO 2 Solution creates the class Dolphin:
- *          extend the class Animal to create a new
- *          water based animal named "Dolphin" which
- *          extends Animal
- *          Extra properties of class "Dolphin" include:
- *          1. color of dolphin
- *          2. swimming speed
- **/
 
-/** TODO 7 Solution is the implementation of the interface
- *         "Swim" and the implementation of the method
- *         "swimming" inside the class.
- *         The todo  states:
- *                          implement the "Swim" interface
- *                          in the "Dolphin" class and the
- *                          "swimming" method in its implementation
- *                          should display the swimming speed as
- *                          "Dolphin: I am swimming at the speed ...."
- *                           where .... is the value of the variable
- *                          "swimmingSpeed"
- **/
 public class Dolphin extends Animal implements Swim{
 
     // property for color of dolphin
@@ -47,12 +27,7 @@ public class Dolphin extends Animal implements Swim{
         this.swimmingSpeed = swimmingSpeed;
     }
 
-    /** TODO 3 Solution :implement the unimplemented methods of
-     *                   "Eat" interface in the class
-     *                   "Tiger" class created in the TODO 1
-     *                   and also in the
-     *                  "Dolphin" class created in TODO 2.
-     */
+  
     @Override
     public void eatingFood() {
         System.out.println("Dolphin: I am eating delicious fish.");
@@ -63,13 +38,11 @@ public class Dolphin extends Animal implements Swim{
         System.out.println("Dolphin: I have eaten fish.");
     }
 
-    /** TODO 3 Solution End **/
-
-    /** TODO 7 Solution **/
+  
     @Override
     public void swimming() {
         System.out.println("Dolphin: I am swimming at the speed " + swimmingSpeed);
     }
-    /** TODO 7 Solution End **/
+
 
 }
